7b0c7e9e51b88517c81ebcaf128db767e318af47
Timmy Jose <zoltan.jose@gmail.com>
2024-11-05 18:36:42 +0530

    wip: expo push notifications setup

---
+++ src/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#c0c0c0',
+    borderRadius: 5,
+    height: 40,
+    width: '40%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default styles
